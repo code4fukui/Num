918ee5164901266d5eb15a49ba6fa25c00b3fe37
Taisuke Fukuno <fukuno@jig.jp>
2023-11-05 07:37:08 +0900

    add fixbig

--- Num.test.js
+++ Num.test.js
@@ -48,3 +48,30 @@ Deno.test("fixfloat", () => {
   t.assertEquals(Num.fixfloat(-1.123, 4), "-1.1230");
   t.assertEquals(Num.fixfloat(-1000000.123, 4), "-1000000.1230");
 });
+Deno.test("fixbig", () => {
+  t.assertEquals(Num.fixbig(1), "1");
+  t.assertEquals(Num.fixbig(10000), "1.00万");
+  t.assertEquals(Num.fixbig(100000), "10.0万");
+  t.assertEquals(Num.fixbig(1000000), "100万");
+  t.assertEquals(Num.fixbig(10000000), "0.10億");
+  t.assertEquals(Num.fixbig(100000000), "1.00億");
+  t.assertEquals(Num.fixbig(1000000000), "10.0億");
+  t.assertEquals(Num.fixbig(10000000000), "100億");
+  t.assertEquals(Num.fixbig(100000000000), "0.10兆");
+});
+Deno.test("fixbig mega", () => {
+  t.assertEquals(Num.fixbig(1, true), "100万");
+  t.assertEquals(Num.fixbig(10, true), "1000万");
+  t.assertEquals(Num.fixbig(100, true), "1.00億");
+  t.assertEquals(Num.fixbig(1000, true), "10.0億");
+  t.assertEquals(Num.fixbig(10000, true), "100億");
+  t.assertEquals(Num.fixbig(100000, true), "0.10兆");
+  t.assertEquals(Num.fixbig(1000000, true), "1.00兆");
+  t.assertEquals(Num.fixbig(10000000, true), "10.0兆");
+  t.assertEquals(Num.fixbig(100000000, true), "100兆");
+  t.assertEquals(Num.fixbig(1000000000, true), "0.10京");
+  t.assertEquals(Num.fixbig(10000000000, true), "1.00京");
+  t.assertEquals(Num.fixbig(100000000000, true), "10.0京");
+  t.assertEquals(Num.fixbig(1000000000000, true), "100京");
+  t.assertEquals(Num.fixbig(10000000000000, true), "1000京");
+});
